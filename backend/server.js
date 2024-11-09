c8aba1409f99fe54baed9fbd47eed8b4a537717a
christiansanchez-SE <christiansanchez714@gmail.com>
2024-11-08 20:42:20 -0500

    added pages to frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,26 @@ const cookieParser = require("cookie-parser");
 // Recieving reqs on cross-origins
 const cors = require("cors");
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+// Express doesn't naturally convert our data to JSON
+app.use(express.json());
+// Middleware [Lets you use urlencoded in postman]
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+
+connectToDb();
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// Logging Middleware
+app.use((req, res, next) => {
+  const time = new Date();
+  console.log(`------- ${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`);
+  if (Object.keys(req.body).length > 0) {
+    console.log("Containing the data:");
+    console.log(`${JSON.stringify(req.body)}`);
+  }
+  next();
+});
